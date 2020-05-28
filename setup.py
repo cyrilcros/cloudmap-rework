48ba5a0321c73e6e32c42effb5e880fc831ad513
Cyril Cros <cyril.cros@polytechnique.org>
2020-05-27 21:26:27 -0400

    Setup structure

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='cloudmap',
+    version='0.0.1',
+    packages=['cloudmap'],
+    url='https://www.hobertlab.org/cloudmap/',
+    license='MIT',
+    author='Hobert lab',
+    author_email='hobertlab@gmail.com',
+    description='Python packaging of CloudMap'
+)
